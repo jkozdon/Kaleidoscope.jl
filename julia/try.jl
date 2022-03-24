52b2b193a911b0afe44381179130816e4e2823df
Jeremy E Kozdon <jekozdon@nps.edu>
2022-03-24 19:54:43 -0400

    Start of ast

--- julia/try.jl
+++ julia/try.jl
@@ -3,13 +3,5 @@ include("ast.jl")
 
 let
     filename = "fib.kl"
-    lex = Lexer(filename)
-
-    while true
-        tok = gettok!(lex)
-        tok.tok == tok_eof && break
-        if tok.tok == tok_number
-        elseif tok.tok == tok_identifier
-        end
-    end
+    parse(filename)
 end
