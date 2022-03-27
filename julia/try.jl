8d21c801f591378bcbcac7c3611f3dcf593e8fcb
Jeremy E Kozdon <jekozdon@nps.edu>
2022-03-27 15:06:00 -0400

    Some simple optimizations

--- julia/try.jl
+++ julia/try.jl
@@ -3,7 +3,28 @@ include("lexer.jl")
 include("ast.jl")
 include("codegen.jl")
 
-let
-    filename = "test.kl"
-    klparse(filename)
+filename = "test2.kl"
+(cg, scope) = klparse(filename)
+# fpm = LLVM.FunctionPassManager(cg.mod)
+# LLVM.instruction_combining!(fpm)
+# fn = LLVM.functions(cg.mod)["foo"]
+# @show fn
+# LLVM.run!(fpm, fn)
+# @show fn
+
+mpm = LLVM.ModulePassManager()
+
+fn = LLVM.functions(cg.mod)["foo"]
+@show fn
+
+for pass in (
+             LLVM.instruction_combining!,
+             LLVM.reassociate!,
+             LLVM.gvn!,
+            )
+
+    pass(mpm)
+    LLVM.run!(mpm, cg.mod)
+    local fn = LLVM.functions(cg.mod)["foo"]
+    @show fn
 end
